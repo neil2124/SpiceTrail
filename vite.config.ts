0d6f487de40389792dcae13d2bf1f539138bfc4c
Neil <neilmassy2001@gmail.com>
2025-11-17 19:25:07 -0600

    Removed prefix in build

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/SpiceTrail/', // Use '/' for custom domains
+  // base: '/SpiceTrail/', // Use '/' for custom domains
 });
